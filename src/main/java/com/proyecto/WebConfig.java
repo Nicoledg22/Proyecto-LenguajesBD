880b6cd96a6ffc85b723b2cda2902eb42718f308
Nicoledg22 <ndiaz10233@ufide.ac.cr>
2023-12-05 23:53:29 -0600

    Proyecto Final Lenguajes de Base de Datos,grupo6

--- src/main/java/com/proyecto/WebConfig.java
+++ src/main/java/com/proyecto/WebConfig.java
@@ -1,5 +1,10 @@
 package com.proyecto;
 
+
+/*
+    GRUPO 6
+*/
+
 import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
